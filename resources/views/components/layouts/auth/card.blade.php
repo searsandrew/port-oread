c7e2bd5d2ed78e61a4293cf9de032598e9714195
Andrew Sears <andrew@mayfifteenth.com>
2025-11-23 21:31:00 -0600

    Add background

--- resources/views/components/layouts/auth/card.blade.php
+++ resources/views/components/layouts/auth/card.blade.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         @include('partials.head')
     </head>
-    <body class="min-h-screen bg-neutral-100 antialiased dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900">
-        <div class="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+    <body class="min-h-screen antialiased">
+        <div class="bg-black flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+            <div class="space"></div>
             <div class="flex w-full max-w-md flex-col gap-6">
                 <a href="{{ route('home') }}" class="flex flex-col items-center gap-2 font-medium" wire:navigate>
                     <span class="flex h-9 w-9 items-center justify-center rounded-md">
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Laravel\Fortify\Features;
 use Livewire\Volt\Volt;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('dashboard');
 })->name('home');
 
 Route::view('dashboard', 'dashboard')
